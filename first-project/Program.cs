542c8fd23fea13698beb61b61842e84412e63ee7
Jorge-RD <j.rodriguez@intellisys.com.do>
2020-10-24 23:02:47 -0400

    C# - Printing the primitive values

--- first-project/Program.cs
+++ first-project/Program.cs
@@ -6,7 +6,24 @@ namespace first_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintPrimitiveValues();
         }
+
+        static void PrintPrimitiveValues()
+        {
+            const int typeInt = 2;
+            const byte typeByte = 2;
+            const float typeFloat = 2f;
+            const char typeChart = 'F';
+            const bool typeBoolean = !false;
+
+            Console.WriteLine($"{typeInt}, {typeInt.GetType()}");
+            Console.WriteLine($"{typeByte}, {typeByte.GetType()}");
+            Console.WriteLine($"{typeFloat}, {typeFloat.GetType()}");
+            Console.WriteLine($"{typeChart}, {typeChart.GetType()}");
+            Console.WriteLine($"{typeBoolean}, {typeBoolean.GetType()}");
+
+        }
+
     }
 }
